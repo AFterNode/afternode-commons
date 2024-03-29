269bd7a39ec664604bf18f83fc940f83631d178c
H3xadecimal <h3xadecimal@afternode.cn>
2024-03-30 00:57:37 +0800

    add tab complete builder

---
+++ bukkit-kotlin/src/main/kotlin/cn/afternode/commons/bukkit/kotlin/TabBuilder.kt
@@ -0,0 +1,29 @@
+package cn.afternode.commons.bukkit.kotlin
+
+import org.bukkit.Bukkit
+
+class TabBuilder {
+    private val list = ArrayList<String>()
+
+    fun add(vararg items: String, prefix: String = "", ignoreCase: Boolean=false): TabBuilder {
+        items.forEach {
+            if (it.startsWith(prefix, ignoreCase)) list.add(it)
+        }
+        return this
+    }
+
+    fun players(prefix: String = "", ignoreCase: Boolean=false): TabBuilder {
+        Bukkit.getOnlinePlayers().forEach {
+            if (it.name.startsWith(prefix, ignoreCase)) list.add(it.name)
+        }
+        return this
+    }
+
+    fun build() = list.toList()
+}
+
+fun tabComplete(block: TabBuilder.() -> Unit): List<String> {
+    val builder = TabBuilder()
+    block(builder)
+    return builder.build()
+}
