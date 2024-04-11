112b3913beddb936c9114a1fa55fdeb7a0e0a520
H3xadecimal <h3xadecimal@afternode.cn>
2024-04-11 23:06:01 +0800

    feat(bukkit-kotlin): Add custom filter to TabBuilder
    Signed-off-by: H3xadecimal <h3xadecimal@afternode.cn>

--- bukkit-kotlin/src/main/kotlin/cn/afternode/commons/bukkit/kotlin/TabBuilder.kt
+++ bukkit-kotlin/src/main/kotlin/cn/afternode/commons/bukkit/kotlin/TabBuilder.kt
@@ -2,19 +2,34 @@ package cn.afternode.commons.bukkit.kotlin
 
 import org.bukkit.Bukkit
 
-class TabBuilder {
+class TabBuilder(
+    val ignoreCase: Boolean = false
+) {
     private val list = ArrayList<String>()
 
-    fun add(vararg items: String, prefix: String = "", ignoreCase: Boolean=false): TabBuilder {
+    fun add(vararg items: String, prefix: String = "", ignoreCase: Boolean = this.ignoreCase): TabBuilder {
         items.forEach {
             if (it.startsWith(prefix, ignoreCase)) list.add(it)
         }
         return this
     }
 
-    fun players(prefix: String = "", ignoreCase: Boolean=false): TabBuilder {
+    fun players(prefix: String = "", ignoreCase: Boolean = this.ignoreCase): TabBuilder =
+        players { it.startsWith(prefix, ignoreCase) }
+
+    fun players(filter: (String) -> Boolean): TabBuilder {
         Bukkit.getOnlinePlayers().forEach {
-            if (it.name.startsWith(prefix, ignoreCase)) list.add(it.name)
+            if (filter(it.name)) this.list.add(it.name)
+        }
+        return this
+    }
+
+    fun worlds(prefix: String, ignoreCase: Boolean = this.ignoreCase): TabBuilder =
+        worlds { it.startsWith(prefix, ignoreCase) }
+
+    fun worlds(filter: (String) -> Boolean): TabBuilder {
+        Bukkit.getWorlds().forEach {
+            if (filter(it.name)) this.list.add(it.name)
         }
         return this
     }
