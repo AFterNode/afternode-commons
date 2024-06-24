34c205b63305726baafa28796a6f09cbbeea117c
H3xadecimal <h3xadecimal@afternode.cn>
2024-06-24 16:07:59 +0800

    build: Adventure API

--- bukkit/src/main/java/module-info.java
+++ bukkit/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module cn.afternode.commons.afternode.commons.bukkit.main {
     requires org.bukkit;
     requires org.reflections;
     requires org.jetbrains.annotations;
+
     requires net.kyori.adventure;
     requires net.kyori.adventure.text.minimessage;
+    requires net.kyori.examination.api;
 }
